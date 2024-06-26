6296d39a375d9577284cf1ed07c89d5f3df7d5e1
tetriv0217 <tetriv0217@gmail.com>
2024-06-26 18:08:31 +0530

    Different methods to inject our Elements.

--- 01viteReact/src/App.jsx
+++ 01viteReact/src/App.jsx
@@ -1,7 +1,9 @@
 function App() {
+  const username = "Tejas"
   return (
-  <h1>Hello world | Tejas </h1>
+  <h1>Hello world | Tejas {username}</h1>
   )
 }
 
 export default App
+//This is a evaluated expression in {} i.e It should hold only the final outcome you want to make them see 
--- 01viteReact/src/main.jsx
+++ 01viteReact/src/main.jsx
@@ -1,14 +1,27 @@
-import React from 'react'
+// import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import Chai from './Chai.jsx'
+// import Chai from './Chai.jsx'
 
+//making a function however you should make it through imports
+// function Myapp(){
+//   return(
+//     <h1>Teajas</h1>
+//   )
+// }
+
+// const reactElement = React.createElement(
+//   'a',
+//   {
+//     href:'https://google.com',
+//     target : '_blank'
+//   },
+//   'Click me to visit google'
+// )
+// const AnotherElement =  (
+//   <a href="https://google.com" target='_blank'>Visit google</a>
+// )
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <>
-    <Chai />
-    <App />
-    </>
-  </React.StrictMode>,
+    <App/>
 )
